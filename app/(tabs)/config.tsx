58fc417b80743cc4236c57b5e1f551b8dce46b6c
Aubin Altieri <aubin.alt@gmail.com>
2024-10-05 10:38:31 +0200

    add(tabs): moove the config page in a tabs

--- app/(tabs)/config.tsx
+++ app/(tabs)/config.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from "react-native";
 import React from "react";
 
-const config = () => {
+const Config = () => {
   return (
     <View className="flex-1 justify-center items-center">
       <Text>Config Page</Text>
     </View>
   );
 };
+
+export default Config;
--- app/index.tsx
+++ app/index.tsx
@@ -7,16 +7,15 @@ import CustomButton from "@/components/CustomButton";
 import backImage from "@/assets/images/young-man-playing-table-tennis-black.jpg";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
+import AppGradient from "@/components/AppGradient";
+
 const App = () => {
   const router = useRouter();
   return (
     <View className="flex-1">
       <ImageBackground source={backImage} resizeMode="cover" className="flex-1">
-        <LinearGradient
-          className="flex-1"
-          colors={["rgba(0, 0, 0.4)", "rgba(0, 0, 0.8)"]}
-        >
-          <SafeAreaView className="flex-1 mx-5 my-8 justify-between">
+        <AppGradient colors={["rgba(0, 0, 0.4)", "rgba(0, 0, 0.8)"]}>
+          <SafeAreaView className="flex-1 px1 justify-between">
             <View>
               <Text className="text-center text-white font-bold text-4xl">
                 Hello World!
@@ -35,7 +34,7 @@ const App = () => {
 
             <StatusBar style="light" />
           </SafeAreaView>
-        </LinearGradient>
+        </AppGradient>
       </ImageBackground>
     </View>
   );
