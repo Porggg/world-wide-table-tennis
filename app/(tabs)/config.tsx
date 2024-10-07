964b842828bda7895e2129125cf0c8325ac4c03a
Aubin Altieri <aubin.alt@gmail.com>
2024-10-07 10:28:21 +0200

    add(api): first use of the api

--- app/(tabs)/config.tsx
+++ app/(tabs)/config.tsx
@@ -1,10 +1,39 @@
-import { Text, View } from "react-native";
-import React from "react";
+import { ActivityIndicator, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+
+type Match = {
+  home_team_id: string;
+};
 
 const Config = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState<Match[]>([]);
+  const getLiveMatches = async () => {
+    try {
+      const response = await fetch(
+        "https://table-tennis.sportdevs.com/matches-live",
+        {
+          headers: {
+            Authorization: `Bearer xXxXxXxXxXxXxXxXxXxX`,
+          },
+        }
+      );
+      const json = await response.json();
+      console.log(json[0].home_team_id);
+      setData(json[0].home_team_id);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getLiveMatches();
+  }, []);
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Config Page</Text>
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator /> : <Text>{data}</Text>}
     </View>
   );
 };
